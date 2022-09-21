d7540802366aa07706b2885b9302a197596f0a0d
cps97 <colinstoutt@gmail.com>
2022-09-20 20:26:51 -0700

    task 1.1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let mainEl = document.querySelector("main");
 console.log(mainEl);
 
-mainEl.classList.add("");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.classList.add(".flex-ctr");
