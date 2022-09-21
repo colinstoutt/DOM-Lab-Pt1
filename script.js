d9f6aea49e39e3da616d37d6c7016645a876f5ec
cps97 <colinstoutt@gmail.com>
2022-09-20 20:51:11 -0700

    finishedpart 1

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 let mainEl = document.querySelector("main");
 console.log(mainEl);
-
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 mainEl.classList.add("flex-ctr");
+
+let topMenuEl = document.getElementById("top-menu");
+console.log(topMenuEl);
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+menuLinks.forEach((item) => {
+  let menuItems = document.createElement("a");
+  menuItems.textContent = item.text;
+  menuItems.setAttribute("href", `/${item.text}`);
+  topMenuEl.append(menuItems);
+  console.log(menuItems);
+});
